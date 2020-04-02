cd7e8cd74826d769ab28157d2b4239b1fc799d4a
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-04-02 15:18:03 -0400

    generate random walls

--- src/algorithms/RandomMaze.js
+++ src/algorithms/RandomMaze.js
@@ -1,5 +1,47 @@
-import React from 'react';
-
 export default function RandomMaze(grid) {
-  return <div></div>;
+  // Find start and end node
+  const [startNode, finishNode] = grid
+    .reduce((a, b) => a.concat(b))
+    .filter(node => node.isStart || node.isFinish);
+  const path = getRandomPath(grid, startNode, finishNode);
+  const newGrid = addRandomWalls(grid, path, startNode, finishNode);
+  return newGrid;
 }
+
+const addRandomWalls = (grid, path, startNode, finishNode) => {
+  for (const row of grid) {
+    for (const node of row) {
+      if (path.includes(node) || node === startNode || node === finishNode)
+        continue;
+      const addWall = Math.floor(Math.random() * 10);
+      if (addWall < 4) {
+        node.isWall = true;
+      }
+    }
+  }
+  return grid;
+};
+
+const getRandomPath = (options, startNode, finishNode) => {
+  const path = [];
+  while (path[path.length - 1] !== finishNode) {
+    const cur = pickRandomNeighbor(options, startNode);
+    path.push(cur);
+    options = options.filter(node => node !== cur);
+    break;
+  }
+  return path;
+};
+
+const pickRandomNeighbor = (options, node) => {
+  console.log(node, node.col, node.row);
+  const neighbors = [];
+  const {col, row} = node;
+  if (row > 0) neighbors.push(options[row - 1][col]);
+  if (row < options.length - 1) neighbors.push(options[row + 1][col]);
+  if (col > 0) neighbors.push(options[row][col - 1]);
+  if (col < options[0].length - 1) neighbors.push(options[row][col + 1]);
+  const neighborNum = Math.floor(Math.random() * (neighbors.length - 1));
+  console.log(neighbors, neighborNum);
+  return neighbors[neighborNum];
+};
